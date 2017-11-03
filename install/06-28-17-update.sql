dd1e42a71235f2cd0bf09bac5b7f70f0e73fb48f
Pierre Tomasina <tomasinap@gmail.com>
2017-11-03 23:55:57 +0100

    remove unexisting column

--- install/06-28-17-update.sql
+++ install/06-28-17-update.sql
@@ -7,7 +7,6 @@ CHANGE `pros_theme` `pros_theme` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_ge
 CHANGE `pros_name` `pros_name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
 CHANGE `pros_email` `pros_email` VARCHAR(150) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
 CHANGE `pros_telephone` `pros_telephone` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
-CHANGE `pros_societe` `pros_societe` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, 
 CHANGE `pros_message` `pros_message` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
 
 ALTER TABLE `melis_cms_prospects` 
